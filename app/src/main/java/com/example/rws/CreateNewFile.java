aadab86dfc979b114f86d2562bddd34998da9ce2
souvikghoshkatwa <souvikghosh687@gmail.com>
2020-07-19 21:08:06 +0530

    create new file added

---
+++ app/src/main/java/com/example/rws/CreateNewFile.java
@@ -0,0 +1,28 @@
+package com.example.rws;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class CreateNewFile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_file);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.fileeditormenu,menu);
+        return (true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/example/rws/SecondActivity.java
+++ app/src/main/java/com/example/rws/SecondActivity.java
@@ -3,16 +3,32 @@ package com.example.rws;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+
+    Button create;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        create = findViewById(R.id.createnewfile);
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SecondActivity.this,CreateNewFile.class));
+                finish();
+            }
+        });
+
     }
 
     @Override
