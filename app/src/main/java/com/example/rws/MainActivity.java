00ee6ba36ed9ee59e83e089b0f63954bdabccf1d
soumen <palsoumen306@gmail.com>
2020-07-19 23:46:15 +0530

    by man

--- app/src/main/java/com/example/rws/MainActivity.java
+++ app/src/main/java/com/example/rws/MainActivity.java
@@ -5,14 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView ig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ig=findViewById(R.id.ig);
+        Animation anim = new AlphaAnimation(1.0f,0.5f);
+        anim.setDuration(700);
+        anim.setRepeatCount(Animation.INFINITE);
+        anim.setRepeatMode(Animation.REVERSE);
+        ig.startAnimation(anim);
         getSupportActionBar().hide();
 
         new Handler().postDelayed(new Runnable() {
