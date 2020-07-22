9644acf59b42105d2f5b66c09ad0298565680519
soumen <palsoumen306@gmail.com>
2020-07-22 11:18:14 +0530

    button add,info

--- app/src/main/java/com/example/rws/AboutActivity.java
+++ app/src/main/java/com/example/rws/AboutActivity.java
@@ -2,13 +2,25 @@ package com.example.rws;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
+    Button apinfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        apinfo=findViewById(R.id.btAppInfo);
+        apinfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AboutActivity.this,appInfo.class));
+                finish();
+            }
+        });
     }
 }
