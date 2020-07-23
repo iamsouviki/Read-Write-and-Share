3d9f1daa9e13377de7ebadcf53e31063e6c178cf
soumen <palsoumen306@gmail.com>
2020-07-23 11:15:40 +0530

    icon

--- app/src/main/java/com/example/rws/contact.java
+++ app/src/main/java/com/example/rws/contact.java
@@ -2,13 +2,60 @@ package com.example.rws;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class contact extends AppCompatActivity {
+    Button iti,souvik,sankar,moti,man;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+        iti=findViewById(R.id.iti);
+        sankar=findViewById(R.id.sankar);
+        souvik=findViewById(R.id.souvik);
+        moti=findViewById(R.id.moti);
+        man=findViewById(R.id.man);
+
+        iti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/iamsouviki/")));
+
+            }
+        });
+        sankar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/spg_45/")));
+
+            }
+        });
+        souvik.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/vksouvik18/")));
+
+            }
+        });
+        moti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/t.r.o.f.e.e/")));
+
+            }
+        });
+        man.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/soumen748/")));
+
+            }
+        });
     }
 }
