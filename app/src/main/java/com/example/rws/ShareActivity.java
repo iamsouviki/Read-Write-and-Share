06a341f278b39cdf962c21e4e4cb240309ff8a92
Souvik Ghosh <souvikghosh687@gmail.com>
2020-08-04 23:06:04 +0530

    bug test

--- app/src/main/java/com/example/rws/ShareActivity.java
+++ app/src/main/java/com/example/rws/ShareActivity.java
@@ -4,14 +4,40 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.net.Uri;
+import android.net.wifi.WifiManager;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
 
 public class ShareActivity extends AppCompatActivity {
     AlertDialog alertDialog;
+    Button sendfile,recievefile,showpic;
+    WifiManager wifiManager;
+    private ArrayList<String> images;
+    GridView gallery;
+    ViewGroup root;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +45,13 @@ public class ShareActivity extends AppCompatActivity {
         setContentView(R.layout.activity_share);
 
         getSupportActionBar().setTitle("Share");
+
+
+        sendfile = findViewById(R.id.send);
+        recievefile = findViewById(R.id.recieve);
+        showpic = findViewById(R.id.showpics);
+        root = findViewById(R.id.sharecontent);
+
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
@@ -36,6 +69,19 @@ public class ShareActivity extends AppCompatActivity {
         }).setCancelable(false);
 
         alertDialog = builder.create();
+        
+        showpic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View picview = LayoutInflater.from(ShareActivity.this).inflate(R.layout.showallpictures, null);
+                gallery = picview.findViewById(R.id.galleryGridView);
+                gallery.setAdapter(new ImageAdapter(this));
+                root.addView(picview);
+                Toast.makeText(getApplicationContext(), images.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
 
     @Override
@@ -57,4 +103,74 @@ public class ShareActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+
+    private class ImageAdapter extends BaseAdapter {
+
+        private Activity context = ShareActivity.this;
+
+        public ImageAdapter(View.OnClickListener localContext) {
+            images = getAllShownImagesPath(context);
+        }
+
+        public int getCount() {
+            return images.size();
+        }
+
+        public Object getItem(int position) {
+            return position;
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public View getView(final int position, View convertView,
+                            ViewGroup parent) {
+            ImageView picturesView;
+            if (convertView == null) {
+                picturesView = new ImageView(context);
+                picturesView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                picturesView
+                        .setLayoutParams(new GridView.LayoutParams(270, 270));
+
+            } else {
+                picturesView = (ImageView) convertView;
+            }
+
+            Glide.with(context).load(images.get(position))
+                    .placeholder(R.drawable.folder).centerCrop()
+                    .into(picturesView);
+
+            return picturesView;
+        }
+
+        private ArrayList<String> getAllShownImagesPath(Activity activity) {
+            Uri uri;
+            Cursor cursor;
+            int column_index_data, column_index_folder_name;
+            ArrayList<String> listOfAllImages = new ArrayList<String>();
+            String absolutePathOfImage = null;
+            uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+
+            String[] projection = { MediaStore.MediaColumns.DATA,
+                    MediaStore.Images.Media.BUCKET_DISPLAY_NAME };
+
+            cursor = activity.getContentResolver().query(uri, projection, null,
+                    null, null);
+
+            column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+            column_index_folder_name = cursor
+                    .getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
+            while (cursor.moveToNext()) {
+                absolutePathOfImage = cursor.getString(column_index_data);
+
+                listOfAllImages.add(absolutePathOfImage);
+            }
+            return listOfAllImages;
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/example/rws/WiFiDirectBroadcastReceiver.java
@@ -0,0 +1,39 @@
+package com.example.rws;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
+
+/**
+ * A BroadcastReceiver that notifies of important Wi-Fi p2p events.
+ */
+public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
+
+    private WifiP2pManager manager;
+    private WifiP2pManager.Channel channel;
+    private ShareActivity activity;
+
+    public WiFiDirectBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel,
+                                       ShareActivity activity) {
+        super();
+        this.manager = manager;
+        this.channel = channel;
+        this.activity = activity;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            // Check to see if Wi-Fi is enabled and notify appropriate activity
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            // Call WifiP2pManager.requestPeers() to get a list of current peers
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+            // Respond to new connection or disconnections
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            // Respond to this device's wifi state changing
+        }
+    }
+}
