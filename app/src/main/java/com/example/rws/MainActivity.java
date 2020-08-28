c3687bb42c742be7008c5c767bf090e8f557a707
Soumen <user@example.com>
2020-08-28 11:21:32 +0530

    n

--- app/src/main/java/com/example/rws/MainActivity.java
+++ app/src/main/java/com/example/rws/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
+        Toast.makeText(getApplicationContext(), "master", Toast.LENGTH_LONG).show();
         getSupportActionBar().setTitle("Read   Write  &  Share");
         requestPermissions(permissions,3);
 
