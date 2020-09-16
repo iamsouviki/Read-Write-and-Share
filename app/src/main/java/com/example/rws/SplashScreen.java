ec1d50544d2bec086f47ec8629c374ed828870dd
souvik <souvikghosh687@gmail.com>
2020-09-16 15:45:38 +0530

    null

--- app/src/main/java/com/example/rws/SplashScreen.java
+++ app/src/main/java/com/example/rws/SplashScreen.java
@@ -17,14 +17,14 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        
         ig=findViewById(R.id.ig);
-        Toast.makeText(getApplicationContext(), "Thanks for installing before publish", Toast.LENGTH_LONG).show();
         Animation anim = new AlphaAnimation(1.0f,0.5f);
         anim.setDuration(700);
         anim.setRepeatCount(Animation.INFINITE);
         anim.setRepeatMode(Animation.REVERSE);
         ig.startAnimation(anim);
-        getSupportActionBar().hide();
 
         new Handler().postDelayed(new Runnable() {
             @Override
