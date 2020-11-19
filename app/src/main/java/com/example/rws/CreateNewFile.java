7f17cbfa65d1279f71818878fa8d447f4affd1f4
Souvik Ghosh <souvikghosh687@gmail.com>
2020-11-19 19:39:24 +0530

    added send recieve activity

--- app/src/main/java/com/example/rws/CreateNewFile.java
+++ app/src/main/java/com/example/rws/CreateNewFile.java
@@ -225,7 +225,7 @@ public class CreateNewFile extends AppCompatActivity {
             case R.id.redofile:
                 if(textcontent.size()>1 && mn+1<textcontent.size()){
                    body.setText(textcontent.get(mn+1));
-                    Toast.makeText(getApplicationContext(), textcontent.get(mn+1)+" "+mn, Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(getApplicationContext(), textcontent.get(mn+1)+" "+mn, Toast.LENGTH_SHORT).show();
                     mn++;
                 }
                 break;
---
+++ app/src/main/java/com/example/rws/SendFile.java
@@ -0,0 +1,14 @@
+package com.example.rws;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SendFile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_file);
+    }
+}
